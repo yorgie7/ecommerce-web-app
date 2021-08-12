8f985e3a408e667ce2e95c3e04825af845cea572
Yogesh Bamanier <bamanieryogesh1@gmail.com>
2021-08-12 14:10:24 +0530

    cart product cart improved

--- src/components/cards/CartProductCard.js
+++ src/components/cards/CartProductCard.js
@@ -6,16 +6,13 @@ import { useDispatch } from 'react-redux';
 
 const CartProductCard = ({ item }) => {
     const dispatch = useDispatch();
-    console.log(item, "hyyy")
     return (
         <div className="cart-item">
-
             <img src={item.productImage} style={{ width: "30%", height: "70px" }} alt="img" />
             <div className="cart-item-details">
                 <div className="">
-                    <p style={{ margin: "4px 0", fontWeight: "600" }}>{item.name}</p>
-                    <p style={{ margin: "4px 0", fontSize: "14px", fontWeight: "500" }}>Rs. {item.price * item.quantity} /-</p>
-
+                    <p style={{ margin: "4px 0", fontWeight: "500" }}>{item.name}</p>
+                    <p style={{ margin: "4px 0", fontSize: "14px", fontWeight: "400" }}>Rs. {item.price * item.quantity} /-</p>
                 </div>
                 <button className="remove-button" onClick={() => dispatch(removeProductFromCart(item.id))}>Remove from Cart </button>
             </div>
--- src/components/cards/ProductCard.js
+++ src/components/cards/ProductCard.js
@@ -11,7 +11,7 @@ const ProductCard = ({ item }) => {
             <img src={item.productImage} style={{ width: "100%", height: "140px" }} alt="img" />
             <div className="row">
                 <p>{item.name}</p>
-                <p>Rs : {item.price}/-</p>
+                <p>Rs. {item.price}/-</p>
             </div>
             <button className="add-button" onClick={() => dispatch(addProductToCart(item))}>Add to Cart</button>
         </div>
